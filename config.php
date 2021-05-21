5541a29c20151b4d5f50aa399d920075e7efea0f
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2021-05-21 11:15:02 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'cms');
+define('DB_USER', 'cms_www');
+define('DB_PASS', 'n7LDiBhtjUK6DtFU');
+
+define('SMTP_HOST', 'smtp.mailgun.org');
+define('SMTP_USER', 'postmaster@sandbox236dfee8390c45018b0b58fdb3497d6d.mailgun.org');
+define('SMTP_PASS', '421e5cc2f9bec784328fb0bf179509a9-6ae2ecad-cb513617');
+
+define('SHOW_ERROR_DETAIL', true);
